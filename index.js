22a48d9ef00ce776766778198e3c38924a51659c
Sarah Denison <sdenison86@gmail.com>
2023-02-10 12:01:29 -0700

    commit tree a6e36c1ab419dd2cde56cbd52c4ae8982f8e0e7a

---
+++ index.js
@@ -0,0 +1,20 @@
+function add() {}
+function subtract() {}
+function multiply() {}
+function divide() {}
+
+function add(a, b) {
+    return (a + b);
+  }
+function subtract(b, a) {
+    return (b - a);
+}
+function multiply(a, b) {
+    return (a * b);
+}
+function divide(a, b) {
+    return (a / b);
+}
+let n = 5 {
+    return (n++);
+}
